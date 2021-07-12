cf41168d7869e696ab68f0c6237a63a51bce0bbc
lkelsall <kelsall.lewis@gmail.com>
2021-07-12 11:38:02 +0100

    allow cross origin resource sharing

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const cors = require("cors");
 const apiRouter = require("./routers/api.router");
 const {
   handle500s,
@@ -8,6 +9,8 @@ const {
 } = require("./error-handling");
 
 const app = express();
+
+app.use(cors());
 
 app.use(express.json());
 
